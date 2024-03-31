16ff819f7add9400058e6a444fbf0c7de3959ff2
Jonatan Cid <86532479+jcidp@users.noreply.github.com>
2024-03-31 16:47:16 -0600

    Add Posts model, controller & view

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,26 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    user = current_user
+    @post = user.posts.build(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def index
+    @posts = Post.all.order(created_by: :desc)
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+end
